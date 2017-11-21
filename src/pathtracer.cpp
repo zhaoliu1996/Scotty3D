50cdf8f535586b666a139f2eecc3ec2fa9f765b3
Zhao Liu <zhaol1@andrew.cmu.edu>
2017-11-21 14:35:34 -0500

    task 7

--- src/pathtracer.cpp
+++ src/pathtracer.cpp
@@ -404,7 +404,11 @@ Spectrum PathTracer::trace_ray(const Ray &r) {
     // TODO (PathTracer):
     // (Task 7) If you have an environment map, return the Spectrum this ray
     // samples from the environment map. If you don't return black.
-    return Spectrum(0, 0, 0);
+      if (envLight != NULL)
+      {
+          return envLight->sample_dir(r);
+      }
+      return Spectrum(0., 0., 0.);
   }
 
 // log ray hit
